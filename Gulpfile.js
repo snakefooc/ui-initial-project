472ee4d7eb3c2daa7f4ed3677279f44291bb94c8
Luis Esquivel <snakefooc@gmail.com>
2016-06-25 12:46:18 -0300

    Update Gulp

---
+++ Gulpfile.js
@@ -0,0 +1,192 @@
+'use strict';
+
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var cache = require('gulp-cached');
+var cssnano = require('gulp-cssnano');
+var fs = require('fs');
+var gulp = require('gulp');
+var handlebars = require('gulp-compile-handlebars');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+var inlinesource = require('gulp-inline-source');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+var layouts = require('handlebars-layouts');
+var plumber = require('gulp-plumber');
+var reload = browserSync.reload;
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var sass = require('gulp-sass');
+var scsslint = require('gulp-scss-lint');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var yaml = require('js-yaml');
+var rimraf = require('rimraf');
+var runSequence = require('run-sequence');
+var path = require('path');
+var notify = require('gulp-notify');
+
+
+handlebars.Handlebars.registerHelper(layouts(handlebars.Handlebars));
+
+gulp.task('sass:lint', function() {
+  gulp.src('./src/assets/sass/*.scss')
+    .pipe(plumber())
+    .pipe(scsslint());
+});
+
+gulp.task('sass:build', function() {
+  gulp.src('./src/assets/sass/**/style.scss')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed',
+      errLogToConsole: false,
+      onError: function(err) {
+          return notify().write(err);
+      }
+    }))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/assets/css/'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('sass:optimized', function() {
+  return gulp.src('./src/assets/sass/**/style.scss')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'compressed',
+    }))
+    .pipe(autoprefixer())
+    .pipe(cssnano({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/assets/css/'));
+});
+
+gulp.task('sass', ['sass:lint', 'sass:build']);
+
+gulp.task('js:build', function() {
+  return gulp.src('src/assets/js/**/*.js')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('js:lint', function() {
+  return gulp.src(['./src/assets/js/**/*.js', '!./src/assets/js/lib/**/*.js', 'Gulpfile.js'])
+    .pipe(plumber())
+      .pipe(jscs())
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('js', ['js:lint', 'js:build']);
+
+gulp.task('images', function() {
+  return gulp.src('src/assets/img/**/*')
+    .pipe(plumber())
+    .pipe(imagemin({
+      progressive: true,
+    }))
+    .pipe(gulp.dest('./dist/assets/img'));
+});
+
+gulp.task('images:optimized', function() {
+  return gulp.src('src/assets/img/**/*')
+    .pipe(plumber())
+    .pipe(imagemin({
+      progressive: true,
+      multipass: true,
+    }))
+    .pipe(gulp.dest('./dist/assets/img'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('src/assets/css/*.css')
+    .pipe(plumber())
+    .pipe(gulp.dest('./dist/assets/css'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/assets/fonts/*')
+    .pipe(plumber())
+    .pipe(gulp.dest('./dist/assets/fonts'));
+});
+
+gulp.task('templates', function() {
+  var templateData = yaml.safeLoad(fs.readFileSync('data.yml', 'utf-8'));
+  var options = {
+    ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
+    batch: ['./src/partials/'],
+    helpers: {
+      capitals: function(str) {
+        return str.toUpperCase();
+      },
+    },
+  };
+
+  return gulp.src('./src/templates/**/*.hbs')
+    .pipe(plumber())
+    .pipe(handlebars(templateData, options))
+    .pipe(rename(function(path) {
+      path.extname = '.html';
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('templates:optimized', ['templates'], function() {
+  return gulp.src('./dist/**/*.html')
+    .pipe(inlinesource())
+    .pipe(replace(/\.\.\//g, ''))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('clean', function(cb) {
+  return rimraf('./dist/', cb);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['./src/templates/**/*.hbs', './src/partials/**/*.hbs'], ['templates'], reload);
+  gulp.watch('./src/assets/sass/**/*.scss', ['sass'], reload);
+  gulp.watch('./src/assets/img/**/*', ['images'], reload);
+  gulp.watch(['./src/assets/js/**/*.js', 'Gulpfile.js'], ['js'], reload);
+});
+
+gulp.task('build', function (cb) {
+  return runSequence('clean', ['sass', 'css', 'images', 'fonts', 'js', 'templates'], cb);
+});
+
+gulp.task('build:optimized', function(cb) {
+  return runSequence('clean',
+    ['sass:optimized', 'css', 'images:optimized', 'fonts', 'js', 'templates:optimized'],
+    cb);
+});
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('serve', ['build'], function() {
+
+  // Serve files from the root of this project
+  browserSync.init(['./dist/**/*'], {
+    ghostMode: {
+      clicks: false,
+      forms: false,
+      scroll: false,
+    },
+    server: {
+      baseDir: './dist',
+    }
+  });
+
+  // add browserSync.reload to the tasks array to make
+  // all browsers reload after tasks are complete.
+  gulp.start(['watch']);
+});
--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# Ui developer Project
-Ui developer Project
+Project Template
+========================================
+
+#### _Because web development doesn't need to be a pain._
+
+The [Minimill](https://minimill.co) project template is the best way to build static sites fast.  With one command, build a static page using [Gulp][gulp], [Handlebars.js][handlebars], and [SCSS][scss].
+
+_Other Versions: [ES6](https://github.com/minimill/project-template/tree/es6), [Minimill internal](https://github.com/minimill/project-template/tree/minimill)_
+
+## Features
+
+- Install the project in just three commands (see "Developing" below).
+- Use [Handlebars.js][handlebars] to keep our HTML organized into templates and partials.
+- Use [SCSS][scss] to keep our CSS organized into logical components.
+- Use [Autoprefixer][autoprefixer] to automatically insert browser prefixes where necessary to handle cross browser compatibility.
+- Use [Browsersync][browsersync] to automatically launch a development version of our website, reload the page whenever we change the HTML, and inject changes to CSS, JavaScript, and images with needing to reload.
+- Use [HTML Minifier][htmlmin], [CSSNano][cssnano], [UglifyJS][uglifyjs], and [ImageMin][imagemin] to compress and optimize our HTML, CSS, JavaScript, and images, respectively.
+- Use [SCSS-Lint][scss-lint], [JSHint][jshint], and [JSCS][jscs] to perform [linting][linting] and style checking on our SCSS and JavaScript files.
+
+All with one command from the terminal:
+
+```bash
+gulp serve
+```
+
+## Setup
+
+Install [npm][npm-install]. Then, install gulp:
+
+```bash
+npm install -g gulp  # May require `sudo`
+```
+
+## Developing
+
+```bash
+npm install            # One time
+gem install scss_lint  # One time
+gulp serve
+```
+
+## Reminders
+
+If you want to use this repo for your next project, make sure to make the following changes:
+
+1. Edit `data.yml`, filling in the HTML metadata associated with your site.
+2. Edit `package.json` providing a `name`, `version`, `description`, `license`, and `repository.url`.
+3. Remove the `.git` folder, so that you start from a fresh commit history.
+4. Edit `LICENSE.md` and `README.md` to your preference.
+
+## Gulp Commands
+
+An overview of Gulp commands available:
+
+### `gulp build`
+
+Builds the site into the `dist` directory.  This includes:
+
+- SCSS w/ linting, sourcemaps and autoprefixing
+- JS linting and uglification
+- Handlebars to HTML
+
+### `gulp build:optimized`
+
+This is used for distributing an optimized version of the site (for deployment).  It includes everything from `gulp build` as well as:
+- SCSS minification
+- CSS / JS inline-sourcing 
+
+### `gulp watch`
+
+Watchs for changes in local files and rebuilds parts of the site as necessary, into the `dist` directory.
+
+### `gulp serve`
+
+Runs `gulp watch` in the background, and serves the `dist` directory at `localhost:3000` with automatic reloading using [Browsersync][browsersync].
+
+## Structure
+
+```bash
+├── Gulpfile.js       # Controls Gulp, used for building the website
+├── README.md         # This file
+├── data.yml          # Metadata associated with the site.
+├── dist/             # Gulp builds the static site into this directory
+├── package.json      # Dependencies
+└── src/              # All source code
+    ├── font/         # Font files
+    ├── img/          # Images and SVGs
+    ├── js/           # Javascript libraries and scripts
+    ├── partials/     # Handlebars HTML partials that are included / extended
+    ├── sass/         # Stylesheets
+    └── templates/    # Handlebars HTML files, one per page on the site.
+```
+
+[autoprefixer]: https://css-tricks.com/autoprefixer/
+[browsersync]: http://www.browsersync.io/
+[cssnano]: http://cssnano.co/
+[gulp]: http://gulpjs.com/
+[handlebars]: http://handlebarsjs.com/
+[htmlmin]: https://github.com/kangax/html-minifier
+[imagemin]: https://github.com/imagemin/imagemin
+[jscs]: http://jscs.info/
+[jshint]: http://jshint.com/
+[linting]: https://en.wikipedia.org/wiki/Lint_%28software%29
+[npm-install]: https://nodejs.org/en/download/
+[uglifyjs]: https://github.com/mishoo/UglifyJS
+[scss]: http://sass-lang.com/
+[scss-lint]: https://github.com/brigade/scss-lint
